07efb4d0c0f680332a1c62380a8ef0f31c636d35
Renat Mavlayviev <AlkatrasMaster@yandex.ru>
2025-03-24 02:13:24 +0400

    Создал класс TaskDto и класс сущности Task

---
+++ src/main/java/org/example/frameworks/dto/TaskDto.java
@@ -0,0 +1,9 @@
+package org.example.frameworks.dto;
+
+public class TaskDto {
+
+    private Long id;
+    private String title;
+    private String description;
+    private Boolean completed;
+}
---
+++ src/main/java/org/example/frameworks/entity/Task.java
@@ -0,0 +1,33 @@
+package org.example.frameworks.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "task")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "completed", columnDefinition = "boolean default false")
+    private Boolean completed;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "update_at", nullable = false)
+    private LocalDateTime updateAt;
+}
